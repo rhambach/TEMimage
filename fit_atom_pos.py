ffd5a7d300a066f96ca9b784eb88847b14c241dd
hambach <hambach@bc94f65d-d228-4274-80c1-0270cc7f3fdd>
2012-01-31 00:07:57 +0000

    first running version for automatic peak determination in energy_dispersion images for Aluminium

--- fit_atom_pos.py
+++ fit_atom_pos.py
@@ -65,8 +65,10 @@ def calc_dual(TilingBrowser,event):
 
 
 # read files (TODO: perform Fourier filter automatically)
-infile="tests/graphene_flower_filtered.dm3";
-rawfile="tests/graphene_flower_raw.dm3";
+#infile="tests/graphene_flower_filtered.dm3";
+#rawfile="tests/graphene_flower_raw.dm3";
+infile="/mnt/win/share/salve/111120_Graphene/HRTEM/graphene_all_filtered.dm3";
+rawfile="/mnt/win/share/salve/111120_Graphene/HRTEM/graphene_all_raw.dm3";
 IN = load(infile);
 INRAW=load(rawfile);
 
